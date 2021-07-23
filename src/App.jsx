f6ec99d0b65a02c4404d6bc70869689dc9afe285
Higino <rodrigohigino123@hotmail.com>
2021-07-22 22:49:41 -0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="box">
+          <h1>Testados</h1>
+          <span>567.505</span>
+        </div>
+        <div className="box">
+          <h1>Descartados</h1>
+          <span>340.384</span>
+        </div>
+        <div className="box">
+          <h1>Confirmados</h1>
+          <span>227.121</span>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
